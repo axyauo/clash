ab1c81cdd6a13302fa9c75935eee32bbea213654
Akshay Pathak <a4akshay12@gmail.com>
2025-12-19 21:32:49 +0530

    Initial commit (frontend + server)

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express,{Application, Request, Response} from 'express';
+import "dotenv/config";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ejs from 'ejs';
+import { sendEmail } from './config/mail.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app: Application = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './views'));
+
+app.get('/', async (req: Request, res: Response) => {
+    const html = await ejs.renderFile(__dirname + '/views/emails/welcome.ejs', { name: "Akshay Pathak" });
+    await sendEmail("akshay.formfees@gmail.com", "Test SMTP", html);
+    return res.json({ message: "Email Sent Successfully" });
+    // return res.render("emails/welcome", { name: "Akshay Pathak" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
